a7f994fe8244addf0b08f2f8c6e1a91e2afe4918
Anton <nachalnikkochengui@gmail.com>
2023-06-23 11:19:38 +0800

    er

--- logger.py
+++ logger.py
@@ -1,4 +1,4 @@
-from Task_8_seminar.data_create import name_data, surname_data, phone_data, address_data
+from data_create import name_data, surname_data, phone_data, address_data
 
 
 def input_data():
